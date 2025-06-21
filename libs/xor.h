36d2272351b649a6d6751ec1e91fc5459da60eb2
DimasikKaras <dbykov141@gmail.com>
2025-06-21 15:43:09 +0700

    first

--- libs/xor.h
+++
@@ -1,9 +0,0 @@
-#ifndef XOR_H
-#define XOR_H
-
-void xorEncryptFile(const std::vector<std::byte>& text, const std::vector<std::byte>& key, std::vector<std::byte>& result);
-void xorDecryptFile(const std::vector<std::byte>& text, const std::vector<std::byte>& key, std::vector<std::byte>& result);
-void xorEncryptString(const std::string& text, const std::vector<std::byte>& key, std::string& result);
-void xorDecryptString(const std::string& text, const std::vector<std::byte>& key, std::string& result);
-
-#endif //XOR_H
